46218f67433b27f248e815955ce97c05c5d99838
Scott <scott.downe@gmail.com>
2021-01-06 22:53:53 -0500

    Fixing url search params stuff.

--- app/middleware.js
+++ app/middleware.js
@@ -3,9 +3,13 @@ import at from './actions.js';
 // This keeps the URL search params in sync.
 function insertUrlParam(key, value) {
   if (history.pushState) {
-    let searchParams = new URLSearchParams(window.location.search);
-    searchParams.set(key, value);
-    let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+    let newurl = new URL(window.location);
+    if (value) {
+      newurl.searchParams.set(key, value);
+    } else {
+      newurl.searchParams.delete(key);
+    }
+    newurl = newurl.toString();
     window.history.pushState({path: newurl}, '', newurl);
   }
 }
@@ -14,7 +18,7 @@ function syncParams({ getState }) {
   return next => action => {
     switch (action.type) {
       case at.ADD_BOON:
-        insertUrlParam(action.data.position, action.data.boon.name);
+        insertUrlParam(action.data.position, action.data.boon?.name);
         break;
     }
     return next(action);
